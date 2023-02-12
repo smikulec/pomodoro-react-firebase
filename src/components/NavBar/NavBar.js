b1275933fe92818634975a43c7d5d350bacafe41
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-02-12 22:53:20 +0100

    Change BEM to module styles

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,73 +1,79 @@
 import { NavLink } from 'react-router-dom';
 import { useCurrentUser } from '../../hooks/useCurrentUser';
-import './NavBar.scss';
+import Button from '@mui/material/Button';
+import styles from './NavBar.module.scss';
+import { styled } from '@mui/material/styles';
+
+const LogoutButton = styled(Button)({
+	marginBottom: '30px',
+});
 
 export const NavBar = () => {
 	const { userData, logoutUser } = useCurrentUser();
 
 	return (
-		<nav className='nav'>
-			<div className='nav__title'>Logged in as {userData.name}</div>
-			<ul className='nav__list'>
-				<li className='nav__list-item'>
+		<nav className={styles.nav}>
+			<div className={styles.navTitle}>Logged in as {userData.name}</div>
+			<ul className={styles.navList}>
+				<li className={styles.navListItem}>
 					<NavLink
 						to='/dashboard'
 						className={({ isActive }) =>
-							isActive ? 'nav__link--active' : 'nav__link'
+							isActive ? styles.navListActive : styles.navLink
 						}>
 						Dashboard
 					</NavLink>
 				</li>
-				<li className='nav__list-item'>
+				<li className={styles.navListItem}>
 					<NavLink
 						to='/timer'
 						className={({ isActive }) =>
-							isActive ? 'nav__link--active' : 'nav__link'
+							isActive ? styles.navListActive : styles.navLink
 						}>
 						Timer
 					</NavLink>
 				</li>
-				<li className='nav__list-item'>
+				<li className={styles.navListItem}>
 					<NavLink
 						to='/new-pomodoro'
 						className={({ isActive }) =>
-							isActive ? 'nav__link--active' : 'nav__link'
+							isActive ? styles.navListActive : styles.navLink
 						}>
 						New pomodoro
 					</NavLink>
 				</li>
-				<li className='nav__list-item'>
+				<li className={styles.navListItem}>
 					<NavLink
 						to='/new-task'
 						className={({ isActive }) =>
-							isActive ? 'nav__link--active' : 'nav__link'
+							isActive ? styles.navListActive : styles.navLink
 						}>
 						New task
 					</NavLink>
 				</li>
-				<li className='nav__list-item'>
+				<li className={styles.navListItem}>
 					<NavLink
 						to='/statistics'
 						className={({ isActive }) =>
-							isActive ? 'nav__link--active' : 'nav__link'
+							isActive ? styles.navListActive : styles.navLink
 						}>
 						Statistics
 					</NavLink>
 				</li>
-				<li className='nav__list-item'>
+				<li className={styles.navListItem}>
 					<NavLink
 						to='/settings'
 						className={({ isActive }) =>
-							isActive ? 'nav__link--active' : 'nav__link'
+							isActive ? styles.navListActive : styles.navLink
 						}>
 						Settings
 					</NavLink>
 				</li>
 			</ul>
 			<div>
-				<button className='nav__btn' onClick={logoutUser}>
+				<LogoutButton variant='contained' onClick={logoutUser}>
 					Logout
-				</button>
+				</LogoutButton>
 			</div>
 		</nav>
 	);
