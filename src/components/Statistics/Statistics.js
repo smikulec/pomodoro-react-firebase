797e0253040b9fdbe3f384c1cde79a69196284b2
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-02-23 23:26:55 +0100

    Add files and some basic layout for statistics page

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material';
+import { NavBar } from '../NavBar';
+
+export const Statistics = () => {
+	return (
+		<>
+			<NavBar />
+			<main>
+				<Typography variant='h2' textAlign={'center'}>
+					Here you can see how much great work you've done
+				</Typography>
+			</main>
+		</>
+	);
+};
