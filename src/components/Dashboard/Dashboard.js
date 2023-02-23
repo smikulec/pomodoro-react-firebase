af03db72ebc3f153a27b227c60726f4b318c1e9a
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-02-23 23:24:54 +0100

    Add changes to the component and change styling to scss module

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,41 @@
 import React from 'react';
-
-import './Dashboard.scss';
-
 import { NavBar } from '../NavBar';
+import { Typography, Container } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { TaskCard } from '../TaskCard';
+import { useTodosList } from '../../hooks/useTodosList';
+import { useState } from 'react';
 
 export function Dashboard() {
+	const { todoList, refreshTodoList } = useTodosList();
+
+	console.log(todoList);
+
 	return (
-		<main className='main'>
+		<>
 			<NavBar />
-			<div className='dashboard'></div>
-		</main>
+			<main>
+				<Container maxWidth='xl'>
+					<Typography variant='h4' sx={{ mb: 5, mt: 10 }}>
+						Hi, Welcome back
+					</Typography>
+					<Typography variant='h5'>What are you up to doing today?</Typography>
+					<Link to='/new-pomodoro'>
+						<Typography variant='h5'>Start working on something new</Typography>
+					</Link>
+					<Typography variant='h5'>
+						...or get back to something you've already worked on
+					</Typography>
+
+					{todoList.map((task) => (
+						<TaskCard
+							taskData={task}
+							key={task.id}
+							onDataChange={refreshTodoList}
+						/>
+					))}
+				</Container>
+			</main>
+		</>
 	);
 }
