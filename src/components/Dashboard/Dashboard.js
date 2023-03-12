c1611ef669bcb0414e8e4ed489f6e4b91a818d68
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-03-12 12:19:12 +0100

    Add changes to styling and text

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import { NavBar } from '../NavBar';
-import { Typography, Container } from '@mui/material';
+import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { TaskCard } from '../TaskCard';
 import { useTodosList } from '../../hooks/useTodosList';
+
+import styles from './Dashboard.module.scss';
 
 export function Dashboard() {
 	const { todoList, refreshTodoList } = useTodosList();
 
 	return (
 		<>
-			<NavBar />
-			<main>
-				<Container maxWidth='xl'>
-					<Typography variant='h4' sx={{ mb: 5, mt: 10 }}>
-						Hi, Welcome back
-					</Typography>
-					<Typography variant='h5'>What are you up to doing today?</Typography>
-					<Link to='/new-pomodoro'>
-						<Typography variant='h5'>Start working on something new</Typography>
-					</Link>
-					<Typography variant='h5'>
-						...or get back to something you've already worked on
-					</Typography>
+			<Typography variant='h4' sx={{ mb: 5 }}>
+				Hi, welcome back!
+			</Typography>
+			<Typography variant='h5' sx={{ pb: 1 }}>
+				What are you up to doing today?
+			</Typography>
+			<Link to='/new-pomodoro' className={styles.link}>
+				<Typography variant='h6' sx={{ py: 2, fontWeight: 400 }}>
+					Start working on something new...
+				</Typography>
+			</Link>
+			<Typography variant='h6' sx={{ pb: 2, fontWeight: 400 }}>
+				...or get back to something you've already worked on
+			</Typography>
 
-					{todoList.map((task) => (
-						<TaskCard
-							taskData={task}
-							key={task.id}
-							onDataChange={refreshTodoList}
-						/>
-					))}
-				</Container>
-			</main>
+			{todoList.map((task) => (
+				<TaskCard
+					taskData={task}
+					key={task.id}
+					onDataChange={refreshTodoList}
+				/>
+			))}
 		</>
 	);
 }
