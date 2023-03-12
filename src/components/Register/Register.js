12a65d4550e61bef36857f95e90ad40e20b565be
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-03-12 12:14:42 +0100

    Rewrite component using MUI components and styling

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,28 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
 import {
-	auth,
+	Button,
+	Divider,
+	IconButton,
+	InputAdornment,
+	TextField,
+	Typography,
+} from '@mui/material';
+import { Stack } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext/AuthProvider';
+import {
 	registerWithEmailAndPassword,
 	signInWithGoogle,
 } from '../../firebase/firebase';
+import { Iconify } from '../Iconify/Iconify';
 
-import './Register.scss';
+import styles from './Register.module.scss';
 
 export function Register() {
 	// TODO: handle error state
-	const [user, loading, error] = useAuthState(auth);
 	const [userData, setUserData] = useState({
 		name: '',
 		email: '',
 		password: '',
 	});
 	const navigate = useNavigate();
+	const [showPassword, setShowPassword] = useState(false);
+	const { user, isLoading } = useAuth();
 
 	useEffect(() => {
-		if (loading) return;
+		if (isLoading) return;
 		if (user) navigate('/dashboard');
-	}, [user, loading, navigate]);
+	}, [user, isLoading, navigate]);
 
 	const handleInputChange = (event) => {
 		setUserData((prevData) => {
@@ -41,49 +51,76 @@ export function Register() {
 
 	return (
 		<>
-			<div className='register__header'>
-				<h1>Are you new here?</h1>
-				<h2>Register so you can track your tasks and pomodoros.</h2>
+			<div className={styles.header}>
+				<Typography variant='h1'>Are you new here?</Typography>
+				<Typography variant='body1' sx={{ my: 5 }}>
+					Register so you can track your tasks and pomodoros.
+				</Typography>
 			</div>
-			<div className='register'>
-				<div className='register__container'>
-					<input
-						type='text'
-						className='register__textBox'
-						name='name'
-						value={userData.name}
-						onChange={handleInputChange}
-						placeholder='Full Name'
-					/>
-					<input
-						type='text'
-						className='register__textBox'
-						name='email'
-						value={userData.email}
-						onChange={handleInputChange}
-						placeholder='E-mail Address'
-					/>
-					<input
-						type='password'
-						className='register__textBox'
-						name='password'
-						value={userData.password}
-						onChange={handleInputChange}
-						placeholder='Password'
-					/>
-					<button className='register__btn' onClick={handleRegister}>
-						Register
-					</button>
-					<button
-						className='register__btn register__google'
-						onClick={signInWithGoogle}>
-						Register with Google
-					</button>
 
-					<div>
-						Already have an account? <Link to='/'>Login</Link> now.
-					</div>
-				</div>
+			<div className={styles.container}>
+				<Stack fullWidth spacing={3}>
+					<Button
+						fullWidth
+						size='large'
+						color='inherit'
+						variant='outlined'
+						onClick={signInWithGoogle}>
+						Register with
+						<Iconify
+							icon='eva:google-fill'
+							color='#DF3E30'
+							width={22}
+							height={22}
+						/>
+					</Button>
+					<Divider sx={{ my: 3 }}>
+						<Typography variant='body2' sx={{ color: 'text.secondary' }}>
+							OR
+						</Typography>
+					</Divider>
+					<TextField
+						name='name'
+						label='Full name'
+						onChange={handleInputChange}
+					/>
+					<TextField
+						name='email'
+						label='Email address'
+						onChange={handleInputChange}
+					/>
+					<TextField
+						name='password'
+						label='Password'
+						type={showPassword ? 'text' : 'password'}
+						onChange={handleInputChange}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position='end'>
+									<IconButton
+										onClick={() => setShowPassword(!showPassword)}
+										edge='end'>
+										<Iconify
+											icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}
+										/>
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+					/>
+					<Button
+						fullWidth
+						size='large'
+						type='submit'
+						variant='contained'
+						onClick={handleRegister}>
+						Register
+					</Button>
+				</Stack>
+
+				<Typography variant='body1' sx={{ my: 5 }}>
+					Already have an account? <Link to='/login'>Login</Link> now.
+				</Typography>
 			</div>
 		</>
 	);
