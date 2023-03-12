176bf881307017f003b19eeb6dcb1d167b1db0c7
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-03-12 12:15:20 +0100

    Add logic to prevent undesired components from flashing

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,12 +1,17 @@
 import { useAuth } from '../../contexts/AuthContext';
 
 import { Navigate, useLocation } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 
 export const ProtectedRoute = ({ children }) => {
-	const { user } = useAuth();
+	const { user, isUserDataLoading } = useAuth();
 	const location = useLocation();
 
-	if (!user) {
+	if (isUserDataLoading) {
+		<CircularProgress />;
+	}
+
+	if (!user && !isUserDataLoading) {
 		return <Navigate to='/login' replace state={{ from: location }} />;
 	}
 
