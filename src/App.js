ee6ded866254add9dd33d3fe19671a8018b94b98
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-04-02 19:02:52 +0200

    Add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import NiceModal from '@ebay/nice-modal-react';
 import { AuthProvider } from './contexts/AuthContext';
 import { RoutesWrapper } from './components/Routes/Routes';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#FDC886',
+		},
+		secondary: {
+			main: '#D55448',
+		},
+	},
+	typography: {
+		fontFamily: ['Poppins', 'sans-serif'].join(','),
+	},
+});
 
 function App() {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<NiceModal.Provider>
 				<Router>
 					<AuthProvider>
@@ -14,7 +29,7 @@ function App() {
 					</AuthProvider>
 				</Router>
 			</NiceModal.Provider>
-		</>
+		</ThemeProvider>
 	);
 }
 
