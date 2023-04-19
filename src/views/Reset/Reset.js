2b6386fc98b02eebbba3b8d7a39f8d2b892693b3
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-04-19 23:23:11 +0200

    Add input field validation and error handling

--- src/views/Reset/Reset.js
+++ src/views/Reset/Reset.js
@@ -1,24 +1,38 @@
 import { Box, Button, Container, TextField, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { auth, sendPasswordReset } from '../../firebase/firebase';
 import styles from './Reset.module.scss';
+import { useAlert } from '../../hooks/useAlert';
+import { useForm } from 'react-hook-form';
 
 export function Reset() {
-	const [email, setEmail] = useState('');
 	const [user, loading] = useAuthState(auth);
 	const navigate = useNavigate();
+	const { showAlert } = useAlert();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
 	useEffect(() => {
 		if (loading) return;
 		if (user) navigate('/dashboard');
 	}, [user, loading, navigate]);
 
-	const handleInputChange = (event) => {
-		setEmail(event.target.value);
+	const handleResetClick = async (data) => {
+		try {
+			const response = await sendPasswordReset(data.email);
+			showAlert.success(response.success);
+		} catch (error) {
+			showAlert.error(
+				'There was an error, please check the email address you entered and try again.'
+			);
+		}
 	};
 
 	return (
@@ -35,7 +49,7 @@ export function Reset() {
 						borderRadius: '35px',
 						backgroundColor: 'white',
 						maxWidth: '550px',
-						padding: { xs: '40px 25px', sm: '80px 50px' },
+						padding: { xs: '40px 25px', sm: '60px 30px' },
 						width: '100%',
 					}}>
 					<Typography
@@ -49,24 +63,29 @@ export function Reset() {
 						Let's help you set a new one.
 					</Typography>
 					<div className={styles.container}>
-						<Stack fullWidth spacing={3}>
-							<TextField
-								name='email'
-								label='Email address'
-								onChange={handleInputChange}
-							/>
-							<Button
-								fullWidth
-								size='large'
-								type='submit'
-								variant='contained'
-								sx={{
-									textTransform: 'unset',
-								}}
-								onClick={() => sendPasswordReset(email)}>
-								Send password reset email
-							</Button>
-						</Stack>
+						<form onSubmit={handleSubmit(handleResetClick)}>
+							<Stack spacing={3}>
+								<TextField
+									name='email'
+									label='Email address'
+									{...register('email', {
+										required: 'This field is required.',
+									})}
+									error={!!errors?.email}
+									helperText={errors?.email ? errors?.email?.message : ' '}
+								/>
+								<Button
+									fullWidth
+									size='large'
+									type='submit'
+									variant='contained'
+									sx={{
+										textTransform: 'unset',
+									}}>
+									Send password reset email
+								</Button>
+							</Stack>
+						</form>
 
 						<Typography variant='body1' sx={{ my: 5 }}>
 							Already have an account?{' '}
