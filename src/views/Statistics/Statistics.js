f8181a40aa565bd0d8cdc63614312edbdf7b20ef
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-04-22 01:04:56 +0200

    Add empty states

--- src/views/Statistics/Statistics.js
+++ src/views/Statistics/Statistics.js
@@ -51,44 +51,56 @@ export const Statistics = () => {
 					<Typography variant='h5' sx={{ pl: 4, pb: 2 }}>
 						Tasks completed
 					</Typography>
-					<Paper sx={{ borderRadius: '28px', padding: '10px 20px' }}>
-						{isLoading ? (
-							<Box sx={{ height: '300px', position: 'relative' }}>
-								<Loader />
-							</Box>
-						) : (
-							completedTasks.map((task, index) => (
-								<TaskCard
-									taskData={task}
-									key={task.id}
-									onDataChange={refreshTodoList}
-									noBottomBorder={index === completedTasks.length - 1}
-								/>
-							))
-						)}
-					</Paper>
+
+					{completedTasks?.length === 0 ? (
+						<Typography>No completed tasks yet.</Typography>
+					) : (
+						<Paper sx={{ borderRadius: '28px', padding: '10px 20px' }}>
+							{isLoading ? (
+								<Box sx={{ height: '300px', position: 'relative' }}>
+									<Loader />
+								</Box>
+							) : (
+								completedTasks.map((task, index) => (
+									<TaskCard
+										taskData={task}
+										key={task.id}
+										onDataChange={refreshTodoList}
+										noBottomBorder={index === completedTasks.length - 1}
+									/>
+								))
+							)}
+						</Paper>
+					)}
 				</Grid>
 				<Grid item xs={12} sm={6} md={6}>
 					<Typography variant='h5' sx={{ pl: 4, pb: 2 }}>
 						Tasks doing
 					</Typography>
-					<Paper sx={{ borderRadius: '28px', padding: '10px 20px' }}>
-						{isLoading ? (
-							<Box sx={{ height: '300px', position: 'relative' }}>
-								<Loader />
-							</Box>
-						) : (
-							uncompletedTasks.map((task, index) => (
-								<TaskCard
-									taskData={task}
-									key={task.id}
-									onDataChange={refreshTodoList}
-									noBottomBorder={index === uncompletedTasks.length - 1}
-									hasMenuButton={true}
-								/>
-							))
-						)}
-					</Paper>
+
+					{uncompletedTasks?.length === 0 ? (
+						<Typography sx={{ paddingLeft: '45px' }}>
+							Nothing to work on here yet.
+						</Typography>
+					) : (
+						<Paper sx={{ borderRadius: '28px', padding: '10px 20px' }}>
+							{isLoading ? (
+								<Box sx={{ height: '300px', position: 'relative' }}>
+									<Loader />
+								</Box>
+							) : (
+								uncompletedTasks.map((task, index) => (
+									<TaskCard
+										taskData={task}
+										key={task.id}
+										onDataChange={refreshTodoList}
+										noBottomBorder={index === uncompletedTasks.length - 1}
+										hasMenuButton={true}
+									/>
+								))
+							)}
+						</Paper>
+					)}
 				</Grid>
 			</Grid>
 		</Box>
