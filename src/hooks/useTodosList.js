90d6bf68d5c09627dbc7b2404645f23e882921cf
Sonja Mikulec <69215212+smikulec@users.noreply.github.com>
2023-04-23 07:55:07 +0000

    comment out code that is not temporarily used

--- src/hooks/useTodosList.js
+++ src/hooks/useTodosList.js
@@ -1,63 +1,64 @@
-import { useCallback, useEffect, useState } from 'react';
-import { auth, db } from '../firebase/firebase';
+import { useCallback, useEffect, useState } from "react";
+import { auth, db } from "../firebase/firebase";
 import {
-	collection,
-	getDocs,
-	addDoc,
-	doc,
-	updateDoc,
-	deleteDoc,
-} from 'firebase/firestore';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useAlert } from './useAlert';
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useAlert } from "./useAlert";
 
 export const useTodosList = () => {
-	const [todos, setTodos] = useState([]);
-	const [wholeTodos, setWholeTodos] = useState();
-	const [user] = useAuthState(auth);
-	const [isDataChanged, setIsDataChanged] = useState(false);
-	const [isDataFetching, setIsDataFetching] = useState(false);
-	const { showAlert } = useAlert();
+  const [todos, setTodos] = useState([]);
+  const [wholeTodos, setWholeTodos] = useState();
+  const [user] = useAuthState(auth);
+  const [isDataChanged, setIsDataChanged] = useState(false);
+  const [isDataFetching, setIsDataFetching] = useState(false);
+  const { showAlert } = useAlert();
 
-	const fetchTodos = useCallback(async () => {
-		try {
-			setIsDataFetching(true);
-			const todosRef = collection(db, 'todos');
-			const todosSnap = await getDocs(todosRef);
-			setWholeTodos(todosSnap?.docs);
-		} catch (e) {
-			console.log('No such document!');
-		} finally {
-			setIsDataFetching(false);
-		}
-	}, []);
+  const fetchTodos = useCallback(async () => {
+    try {
+      setIsDataFetching(true);
+      const todosRef = collection(db, "todos");
+      const todosSnap = await getDocs(todosRef);
+      setWholeTodos(todosSnap?.docs);
+    } catch (e) {
+      console.log("No such document!");
+    } finally {
+      setIsDataFetching(false);
+    }
+  }, []);
 
-	useEffect(() => {
-		if (!user?.uid) {
-			return;
-		}
+  useEffect(() => {
+    if (!user?.uid) {
+      return;
+    }
 
-		fetchTodos();
-	}, [user?.uid, fetchTodos, isDataChanged]);
+    fetchTodos();
+  }, [user?.uid, fetchTodos, isDataChanged]);
 
-	useEffect(() => {
-		if (!wholeTodos) {
-			return;
-		}
+  useEffect(() => {
+    if (!wholeTodos) {
+      return;
+    }
 
-		const newTodos = [];
-		wholeTodos.forEach((doc) => {
-			if (doc.data().userId === user?.uid) {
-				const data = {
-					...doc.data(),
-					id: doc.id,
-				};
-				newTodos.push(data);
-			}
-		});
-		setTodos(newTodos);
-	}, [wholeTodos, user?.uid]);
+    const newTodos = [];
+    wholeTodos.forEach((doc) => {
+      if (doc.data().userId === user?.uid) {
+        const data = {
+          ...doc.data(),
+          id: doc.id,
+        };
+        newTodos.push(data);
+      }
+    });
+    setTodos(newTodos);
+  }, [wholeTodos, user?.uid]);
 
+  /*
 	const addToDoToCollection = async (data) => {
 		return new Promise(async (reject, resolve) => {
 			try {
@@ -73,60 +74,60 @@ export const useTodosList = () => {
 			}
 		});
 	};
+*/
+  const addTodo = async (data) => {
+    try {
+      const docRef = await addDoc(collection(db, "todos"), {
+        ...data,
+        userId: user?.uid,
+        createdAt: new Date().toISOString(),
+      });
+      return docRef.id;
+    } catch (error) {
+      throw new Error("Something went wrong, please try again");
+    }
+  };
 
-	const addTodo = async (data) => {
-		try {
-			const docRef = await addDoc(collection(db, 'todos'), {
-				...data,
-				userId: user?.uid,
-				createdAt: new Date().toISOString(),
-			});
-			return docRef.id;
-		} catch (error) {
-			throw new Error('Something went wrong, please try again');
-		}
-	};
+  const updateTodo = async (data) => {
+    setIsDataChanged(false);
+    const documentId = data.id;
+    delete data.id;
+    const docRef = doc(db, "todos", documentId);
+    try {
+      await updateDoc(docRef, {
+        ...data,
+        userId: user.uid,
+      });
+      showAlert.success("Task successfully updated!");
+      fetchTodos();
+    } catch (e) {
+      console.error("Error updating document: ", e);
+      showAlert.error("Something went wrong, please try again");
+    }
+  };
 
-	const updateTodo = async (data) => {
-		setIsDataChanged(false);
-		const documentId = data.id;
-		delete data.id;
-		const docRef = doc(db, 'todos', documentId);
-		try {
-			await updateDoc(docRef, {
-				...data,
-				userId: user.uid,
-			});
-			showAlert.success('Task successfully updated!');
-			fetchTodos();
-		} catch (e) {
-			console.error('Error updating document: ', e);
-			showAlert.error('Something went wrong, please try again');
-		}
-	};
+  const deleteTodo = async (documentId) => {
+    const docRef = doc(db, "todos", documentId);
+    try {
+      await deleteDoc(docRef);
+      showAlert.success("Task successfully deleted!");
+      fetchTodos();
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      showAlert.error("Something went wrong, please try again");
+    }
+  };
 
-	const deleteTodo = async (documentId) => {
-		const docRef = doc(db, 'todos', documentId);
-		try {
-			await deleteDoc(docRef);
-			showAlert.success('Task successfully deleted!');
-			fetchTodos();
-		} catch (e) {
-			console.error('Error adding document: ', e);
-			showAlert.error('Something went wrong, please try again');
-		}
-	};
+  const refreshTodoList = () => {
+    fetchTodos();
+  };
 
-	const refreshTodoList = () => {
-		fetchTodos();
-	};
-
-	return {
-		todoList: todos,
-		addTodo,
-		updateTodo,
-		deleteTodo,
-		refreshTodoList,
-		isLoading: isDataFetching,
-	};
+  return {
+    todoList: todos,
+    addTodo,
+    updateTodo,
+    deleteTodo,
+    refreshTodoList,
+    isLoading: isDataFetching,
+  };
 };
--- src/views/Session/Session.js
+++ src/views/Session/Session.js
@@ -1,153 +1,157 @@
 import {
-	Button,
-	Typography,
-	Box,
-	Paper,
-	InputLabel,
-	Divider,
-	TextField,
-	Tooltip,
-} from '@mui/material';
-import { Stack } from '@mui/system';
-import { useState } from 'react';
-import { useAlert, useTodosList } from '../../hooks';
-import { CounterWizard, Iconify } from '../../components';
-import { FormProvider, useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+  Button,
+  Typography,
+  Box,
+  Paper,
+  InputLabel,
+  Divider,
+  TextField,
+  Tooltip,
+} from "@mui/material";
+import { Stack } from "@mui/system";
+import { useState } from "react";
+import { useAlert, useTodosList } from "../../hooks";
+import { CounterWizard, Iconify } from "../../components";
+import { FormProvider, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 export const Session = () => {
-	const [sessionLength, setSessionLength] = useState(25);
-	const [shortBreakLength, setShortBreakLength] = useState(5);
-	const [longBreakLength, setLongBreakLength] = useState(15);
-	const [rounds, setRounds] = useState(4);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [shortBreakLength, setShortBreakLength] = useState(5);
+  const [longBreakLength, setLongBreakLength] = useState(15);
+  const [rounds, setRounds] = useState(4);
 
-	const { showAlert } = useAlert();
+  const { showAlert } = useAlert();
 
-	const navigate = useNavigate();
-	const methods = useForm();
+  // const navigate = useNavigate();
+  const methods = useForm();
 
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = methods;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
 
-	const { addTodo } = useTodosList();
+  const { addTodo } = useTodosList();
 
-	const handleSaveData = async (data) => {
-		try {
-			const preparedData = {
-				taskName: data.taskName,
-				rounds: rounds,
-				sessionLength: sessionLength,
-				shortBreakLength: shortBreakLength,
-				longBreakLength: longBreakLength,
-				totalTime: 0,
-				isCompleted: false,
-			};
-			const docId = await addTodo(preparedData);
+  const handleSaveData = async (data) => {
+    try {
+      const preparedData = {
+        taskName: data.taskName,
+        rounds: rounds,
+        sessionLength: sessionLength,
+        shortBreakLength: shortBreakLength,
+        longBreakLength: longBreakLength,
+        totalTime: 0,
+        isCompleted: false,
+      };
+      // const docId = await addTodo(preparedData);
 
-			showAlert.success('Task successfully created!');
+      showAlert.success("Task successfully created!");
 
-			// TODO implement redirect with task data
-			// if (docId) {
-			// 	navigate('/timer', { state: { data: taskData } });
-			// }
-		} catch (error) {
-			showAlert.error(error.message);
-		}
-	};
+      // TODO implement redirect with task data
+      // if (docId) {
+      // 	navigate('/timer', { state: { data: taskData } });
+      // }
+    } catch (error) {
+      showAlert.error(error.message);
+    }
+  };
 
-	return (
-		<FormProvider {...methods}>
-			<form onSubmit={handleSubmit(handleSaveData)}>
-				<Stack direction='row'>
-					<Typography variant='h4' sx={{ mb: 5, fontWeight: 700 }}>
-						Create New Pomodoro
-					</Typography>
-					<Tooltip
-						placement='right'
-						title='The Pomodoro Technique is a time management method that helps you
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(handleSaveData)}>
+        <Stack direction="row">
+          <Typography variant="h4" sx={{ mb: 5, fontWeight: 700 }}>
+            Create New Pomodoro
+          </Typography>
+          <Tooltip
+            placement="right"
+            title='The Pomodoro Technique is a time management method that helps you
 							break down your work into focused intervals, usually 25 minutes in
 							length, followed by a short break. These intervals are called
 							"pomodoros". After completing a certain number of work intervals,
 							typically four, a longer break of around 15-30 minutes is taken to
-							recharge and improve productivity for the next round.'>
-						<Iconify
-							icon='material-symbols:info-outline-rounded'
-							sx={{ ml: 1 }}
-						/>
-					</Tooltip>
-				</Stack>
-				<Box maxWidth='sm' sx={{ mb: 6 }}>
-					<Typography variant='h6'>What would you like to work on?</Typography>
-				</Box>
-				<Paper
-					sx={{
-						maxWidth: '450px',
-						borderRadius: '28px',
-						padding: { xs: '20px', md: '40px' },
-					}}>
-					<InputLabel
-						sx={{ color: 'primary.contrastText', fontWeight: 600, pb: 1 }}>
-						Task name
-					</InputLabel>
-					<TextField
-						fullWidth
-						name='taskName'
-						variant='standard'
-						{...register('taskName', { required: true })}
-						error={!!errors?.taskName}
-						helperText={errors?.taskName?.message}
-					/>
-					<Divider
-						sx={{
-							width: '100px',
-							borderColor: 'primary.main',
-							borderBottomWidth: '4px',
-							my: 4,
-						}}
-					/>
-					<Stack>
-						<CounterWizard
-							title='Work session length'
-							time={sessionLength}
-							onLengthChange={setSessionLength}
-							info='The number of rounds (work intervals) you want to complete before taking a long break.'
-						/>
-						<CounterWizard
-							title='Pomodoro rounds'
-							time={rounds}
-							onLengthChange={setRounds}
-							info='The length of a single work interval, typically 25 minutes.'
-						/>
-						<CounterWizard
-							title='Short break length'
-							time={shortBreakLength}
-							onLengthChange={setShortBreakLength}
-							info='The length of the break you take after completing all of the work rounds.'
-						/>
-						<CounterWizard
-							title='Long break length'
-							time={longBreakLength}
-							onLengthChange={setLongBreakLength}
-							info='The length of the break you take between each work round.'
-						/>
-					</Stack>
-					<Button
-						fullWidth
-						variant='contained'
-						type='submit'
-						sx={{
-							textTransform: 'unset',
-							fontWeight: 700,
-							mt: 5,
-							py: 1.5,
-						}}>
-						Add new pomodoro
-					</Button>
-				</Paper>
-			</form>
-		</FormProvider>
-	);
+							recharge and improve productivity for the next round.'
+          >
+            <Iconify
+              icon="material-symbols:info-outline-rounded"
+              sx={{ ml: 1 }}
+            />
+          </Tooltip>
+        </Stack>
+        <Box maxWidth="sm" sx={{ mb: 6 }}>
+          <Typography variant="h6">What would you like to work on?</Typography>
+        </Box>
+        <Paper
+          sx={{
+            maxWidth: "450px",
+            borderRadius: "28px",
+            padding: { xs: "20px", md: "40px" },
+          }}
+        >
+          <InputLabel
+            sx={{ color: "primary.contrastText", fontWeight: 600, pb: 1 }}
+          >
+            Task name
+          </InputLabel>
+          <TextField
+            fullWidth
+            name="taskName"
+            variant="standard"
+            {...register("taskName", { required: true })}
+            error={!!errors?.taskName}
+            helperText={errors?.taskName?.message}
+          />
+          <Divider
+            sx={{
+              width: "100px",
+              borderColor: "primary.main",
+              borderBottomWidth: "4px",
+              my: 4,
+            }}
+          />
+          <Stack>
+            <CounterWizard
+              title="Work session length"
+              time={sessionLength}
+              onLengthChange={setSessionLength}
+              info="The number of rounds (work intervals) you want to complete before taking a long break."
+            />
+            <CounterWizard
+              title="Pomodoro rounds"
+              time={rounds}
+              onLengthChange={setRounds}
+              info="The length of a single work interval, typically 25 minutes."
+            />
+            <CounterWizard
+              title="Short break length"
+              time={shortBreakLength}
+              onLengthChange={setShortBreakLength}
+              info="The length of the break you take after completing all of the work rounds."
+            />
+            <CounterWizard
+              title="Long break length"
+              time={longBreakLength}
+              onLengthChange={setLongBreakLength}
+              info="The length of the break you take between each work round."
+            />
+          </Stack>
+          <Button
+            fullWidth
+            variant="contained"
+            type="submit"
+            sx={{
+              textTransform: "unset",
+              fontWeight: 700,
+              mt: 5,
+              py: 1.5,
+            }}
+          >
+            Add new pomodoro
+          </Button>
+        </Paper>
+      </form>
+    </FormProvider>
+  );
 };
